2c46a7f8c4375867f72fa0936f30378378f35fd1
Aditya Sinha <adityasinha16876@gmail.com>
2026-06-19 00:00:52 +0530

    deploying frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,5 @@
 const isProd = true;
 
-export const server = isProd ? "https://letschat-backend-dw45.onrender.com" : "http://localhost:8000"
+const server = isProd ? "https://letschat-backend-dw45.onrender.com" : "http://localhost:8000"
+
+export default server;
